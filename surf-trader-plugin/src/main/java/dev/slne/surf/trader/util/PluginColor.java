d6c2f89e35fce6228049765521970e86fb83ab60
TheBjoRedCraft <red.dev@hexle.at>
2025-01-21 23:29:03 +0100

    feat: add plugincolor

---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/util/PluginColor.java
@@ -0,0 +1,15 @@
+package dev.slne.surf.trader.util;
+
+import net.kyori.adventure.text.format.TextColor;
+
+public class PluginColor {
+  public static final TextColor BLUE_DARK = TextColor.fromHexString("#3b92d1");
+  public static final TextColor BLUE = TextColor.fromHexString("#40d1db");
+  public static final TextColor LIGHT_BLUE = TextColor.fromHexString("#96cfe7");
+  public static final TextColor BLUE_MID = TextColor.fromHexString("#4bb8f8");
+  public static final TextColor RED = TextColor.fromHexString("#e94a30");
+  public static final TextColor GOLD = TextColor.fromHexString("#fba24e");
+  public static final TextColor LIGHT_GRAY = TextColor.fromHexString("#d3d3d3");
+  public static final TextColor DARK_GRAY = TextColor.fromHexString("#a9a9a9");
+  public static final TextColor WHITE = TextColor.fromHexString("#ffffff");
+}

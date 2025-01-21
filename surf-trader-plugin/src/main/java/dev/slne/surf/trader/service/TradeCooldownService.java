f6ab9ff3ed82e2353dd2ed1af0a9d3ddaceecd87
TheBjoRedCraft <red.dev@hexle.at>
2025-01-21 18:49:27 +0100

    chore: cooldown + requirement service

---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/service/TradeCooldownService.java
@@ -0,0 +1,48 @@
+package dev.slne.surf.trader.service;
+
+import dev.slne.surf.trader.SurfTrader;
+import dev.slne.surf.trader.obj.ShopTrade;
+import lombok.Getter;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+public class TradeCooldownService {
+  @Getter
+  private static final TradeCooldownService instance = new TradeCooldownService();
+
+  public void setCooldown(Player player, ShopTrade trade) {
+    PersistentDataContainer container = player.getPersistentDataContainer();
+    NamespacedKey key = this.getTradeKey(trade);
+
+    container.set(key, PersistentDataType.LONG, System.currentTimeMillis() + trade.getCooldown());
+  }
+
+  public void resetCooldown(Player player, ShopTrade trade) {
+    PersistentDataContainer container = player.getPersistentDataContainer();
+    NamespacedKey key = this.getTradeKey(trade);
+
+    container.set(key, PersistentDataType.LONG, 0L);
+  }
+
+  public long getCooldown(Player player, ShopTrade trade) {
+    PersistentDataContainer container = player.getPersistentDataContainer();
+    NamespacedKey key = this.getTradeKey(trade);
+
+    Long cooldown = container.get(key, PersistentDataType.LONG);
+    return cooldown != null ? cooldown : 0;
+  }
+
+  public boolean isCooldown(Player player, ShopTrade trade) {
+    PersistentDataContainer container = player.getPersistentDataContainer();
+    NamespacedKey key = this.getTradeKey(trade);
+
+    Long cooldown = container.get(key, PersistentDataType.LONG);
+    return cooldown != null && cooldown > System.currentTimeMillis();
+  }
+
+  private NamespacedKey getTradeKey(ShopTrade trade) {
+    return new NamespacedKey(SurfTrader.getInstance(), trade.getID());
+  }
+}
---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/service/TradeRequirementService.java
@@ -0,0 +1,44 @@
+package dev.slne.surf.trader.service;
+
+import dev.slne.surf.trader.obj.ShopTrade;
+import dev.slne.surf.trader.obj.ShopTradeCurrency;
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class TradeRequirementService {
+  @Getter
+  private static final TradeRequirementService instance = new TradeRequirementService();
+
+  public boolean hasRequirements(Player player, ShopTrade trade) {
+    Object required = trade.getPrize();
+
+    if(required instanceof ItemStack stack) {
+      return player.getInventory().containsAtLeast(stack, stack.getAmount());
+    } else if(required instanceof Integer amount) {
+      if(trade.getCurrency().equals(ShopTradeCurrency.CAST_COINS)) {
+        /* Add Transaction API for CastCoins */
+      } else if(trade.getCurrency().equals(ShopTradeCurrency.EVENT_COINS)) {
+        /* Add Transaction API for EventCoins */
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  public void removeRequirements(Player player, ShopTrade trade) {
+    Object required = trade.getPrize();
+
+    if(required instanceof ItemStack stack) {
+      player.getInventory().removeItem(stack);
+    } else if(required instanceof Integer amount) {
+      if(trade.getCurrency().equals(ShopTradeCurrency.CAST_COINS)) {
+        /* Add Transaction API for CastCoins */
+      } else if(trade.getCurrency().equals(ShopTradeCurrency.EVENT_COINS)) {
+        /* Add Transaction API for EventCoins */
+      }
+    }
+  }
+}

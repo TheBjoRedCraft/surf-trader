0e4cb253e3dbe5fc1e2500ad04f11ba9d62a1917
TheBjoRedCraft <red.dev@hexle.at>
2025-01-21 18:47:35 +0100

    chore: trader interfaces

---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/obj/MenuableTrader.java
@@ -0,0 +1,7 @@
+package dev.slne.surf.trader.obj;
+
+import org.bukkit.entity.Player;
+
+public interface MenuableTrader {
+  void openMenu(Player player);
+}
---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/obj/SpawnableTrader.java
@@ -0,0 +1,7 @@
+package dev.slne.surf.trader.obj;
+
+import org.bukkit.Location;
+
+public interface SpawnableTrader {
+  void spawn(Location location);
+}

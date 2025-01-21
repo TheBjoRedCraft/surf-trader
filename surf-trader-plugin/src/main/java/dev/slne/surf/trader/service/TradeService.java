7425ac21bb28c0e00008e048fb68d5c87866101e
TheBjoRedCraft <red.dev@hexle.at>
2025-01-21 18:48:49 +0100

    chore: trade service

---
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/service/TradeService.java
@@ -0,0 +1,64 @@
+package dev.slne.surf.trader.service;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import dev.slne.surf.trader.obj.ShopTrade;
+
+import dev.slne.surf.trader.util.TimeUtil;
+import java.time.Duration;
+import lombok.Getter;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+@Getter
+public class TradeService {
+  @Getter
+  private static final TradeService instance = new TradeService();
+  private final TradeCooldownService cooldownService = TradeCooldownService.getInstance();
+  private final TradeRequirementService requirementService = TradeRequirementService.getInstance();
+
+  private final LoadingCache<ItemStack, ShopTrade> tradeCache = Caffeine
+      .newBuilder()
+      .expireAfterWrite(Duration.ofMinutes(30))
+      .build(this::loadTrade);
+
+  public void buy(Player player, ShopTrade trade) {
+    if (cooldownService.isCooldown(player, trade)) {
+      player.sendActionBar(Component.text("Bitte warte noch ", NamedTextColor.GRAY).append(TimeUtil.calcEnding(cooldownService.getCooldown(player, trade), NamedTextColor.GOLD)));
+      return;
+    }
+
+    if(!requirementService.hasRequirements(player, trade)) {
+      return;
+    }
+
+    requirementService.removeRequirements(player, trade);
+
+    if (player.getInventory().firstEmpty() == -1) {
+      player.getWorld().dropItem(player.getLocation(), trade.getItem()).setOwner(player.getUniqueId());
+    } else {
+      player.getInventory().addItem(trade.getItem());
+    }
+
+    cooldownService.setCooldown(player, trade);
+
+    player.sendMessage(Component.text("Du hast ", NamedTextColor.GOLD).append(trade.getName()).append(Component.text(" gekauft", NamedTextColor.GOLD)));
+  }
+
+  public ShopTrade getTrade(ItemStack stack) {
+    return tradeCache.get(stack);
+  }
+
+  private ShopTrade loadTrade(ItemStack stack) {
+    for (ShopTrade registeredTrade : RegistryService.getInstance().getRegisteredTrades()) {
+      if (registeredTrade.getItem().isSimilar(stack)) {
+        return registeredTrade;
+      }
+    }
+
+    return null;
+  }
+}

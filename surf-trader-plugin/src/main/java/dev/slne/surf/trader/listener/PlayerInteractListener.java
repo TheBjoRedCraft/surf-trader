2da0da6e91fb6dabfe83515c937ac6574a6e5400
TheBjoRedCraft <red.dev@hexle.at>
2025-02-01 01:20:06 +0100

    fix: clean

--- surf-trader-plugin/src/main/java/dev/slne/surf/trader/listener/PlayerInteractListener.java
+++ surf-trader-plugin/src/main/java/dev/slne/surf/trader/listener/PlayerInteractListener.java
@@ -5,13 +5,10 @@ import dev.slne.surf.trader.SurfTrader;
 import dev.slne.surf.trader.impl.SpawnTrader;
 import dev.slne.surf.trader.obj.ShopTrader;
 
-import lol.pyr.znpcsplus.api.NpcApi;
-import lol.pyr.znpcsplus.api.NpcApiProvider;
 import lol.pyr.znpcsplus.api.entity.EntityProperty;
 import lol.pyr.znpcsplus.api.event.NpcInteractEvent;
 import lol.pyr.znpcsplus.api.npc.Npc;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;

ac96554bed0da247958c32d74e604cc5513c8d27
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-19 14:06:59 +0100

    feat: Implement cooldown management for SurfTrade functionality

--- surf-trader-api/src/main/kotlin/dev/slne/surf/trader/api/trade/SurfTrade.kt
+++ surf-trader-api/src/main/kotlin/dev/slne/surf/trader/api/trade/SurfTrade.kt
@@ -3,12 +3,16 @@ package dev.slne.surf.trader.api.trade
 import dev.slne.surf.trader.api.requirement.SurfTradeRequirement
 import it.unimi.dsi.fastutil.objects.ObjectList
 import net.kyori.adventure.text.Component
+import org.bukkit.NamespacedKey
+import java.util.UUID
 
 data class SurfTrade (
-    val id : Int,
+    val id : UUID,
     val name: String,
     val displayName: Component,
     val description: Component,
+
+    val cooldownKey: NamespacedKey = NamespacedKey("surf-trader", "trade-cooldown-$id"),
 
     val cooldown: Long,
     var requirements: ObjectList<SurfTradeRequirement>
--- surf-trader-bukkit/src/main/kotlin/dev/slne/surf/trader/bukkit/implementation/BukkitCooldownService.kt
+++ surf-trader-bukkit/src/main/kotlin/dev/slne/surf/trader/bukkit/implementation/BukkitCooldownService.kt
@@ -5,18 +5,25 @@ import dev.slne.surf.trader.api.trade.SurfTrade
 import dev.slne.surf.trader.core.service.CooldownService
 import net.kyori.adventure.util.Services.Fallback
 import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
 
 @AutoService(CooldownService::class)
 class BukkitCooldownService(): CooldownService, Fallback {
     override fun getCooldown(player: Player, trade: SurfTrade): Long {
-        TODO("Not yet implemented")
+        val pdc = player.persistentDataContainer
+        val key = trade.cooldownKey
+
+        return pdc.getOrDefault(key, PersistentDataType.LONG, 0L)
     }
 
     override fun setCooldown(player: Player, trade: SurfTrade, cooldown: Long) {
-        TODO("Not yet implemented")
+        val pdc = player.persistentDataContainer
+        val key = trade.cooldownKey
+
+        pdc.set(key, PersistentDataType.LONG, cooldown)
     }
 
     override fun isOnCooldown(player: Player, trade: SurfTrade): Boolean {
-        TODO("Not yet implemented")
+        return this.getCooldown(player, trade) > System.currentTimeMillis()
     }
 }
